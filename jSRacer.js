c11047741c4f0dcbc32aec6f5c4417ba9fd1596d
awiles808 <awiles808@gmail.com>
2018-03-25 22:58:44 -0600

    onkeypress Function

--- jSRacer.js
+++ jSRacer.js
@@ -2,16 +2,19 @@ document.addEventListener('DOMContentLoaded', function() { });
 
 alert("Are you ready to rumble!");
 
-
+//Calls for Player1
 const player1 = ()=> {
-    document.getElementById('player1').keyDown();
+    document.getElementById('player1').press(97);
 };
+//Calls for Player2
 
 const player2 = ()=> {
-    document.getElementById('player1').keyDown();
+    document.getElementById('player1').press(108);
 };
 
-
+document.onkeypress = function(e) {
+    e = e || window.event; var charCode = e.charCode || e.keyCode, character = String.fromCharCode(charCode);
+        console.log(charCode); };
 
 
 
