4b38f2c4616b561e357de659d45cce0e103b37fe
awiles808 <awiles808@gmail.com>
2018-03-27 17:16:39 -0600

    Racers move and stops at finish line.The race is on its way

--- jSRacer.js
+++ jSRacer.js
@@ -2,26 +2,50 @@
 
 const player1 = document.getElementById('player1');
 let margValue = 0;
+
 document.addEventListener('keydown',(event) => {
  //console.log(e.keycode)
 console.log("Button is pushed")
 });
 
 
+
 function move(e) {
 
   if(e.keyCode == 39){
-
         margValue += 30;
-//player1.style.left = player1style.Left + "px";
+        player1.style.left = margValue + ("px");
+        if(margValue >= 800){
+            alert("winner winner chicken dinner");
+        }
+}
+   if(e.keyCode == 65){
+        margValue += 30;
+        player2.style.left = margValue + ("px");
+        if(margValue >= 800){
+            alert("winner winner chicken dinner");
+}
 }
 };
 
-console.log(move());
+
+document.onkeydown = move;
 
 
 
 
+
+
+
+
+//  if(e.keyCode == 65){
+//
+//        margValue += 30;
+//        player2.style.left = margValue + "px";
+//        if(player2 >= 800){
+//            alert("winner")
+//}
+//}
 
 
 
