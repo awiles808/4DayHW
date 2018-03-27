50e5879a41f7ea7fd7b84c8448ffe60a97379eb7
awiles808 <awiles808@gmail.com>
2018-03-27 10:46:35 -0600

    Closer and Closer

--- jSRacer.js
+++ jSRacer.js
@@ -1,30 +1,23 @@
-
- document.addEventListener('DOMContentLoaded', function() { });
-
  alert("Are you ready to rumble!");
 
-
-
-
-
-
- alert("Are you ready to rumble!");
-
-const player1 = document.getElementById();
+const player1 = document.getElementById('player1');
 let margValue = 0;
-document.addEventListener('keydown',(e) => {
- //console.log(event keycode)
-
-  if(e.keyCode==39)
-
-    margValue += 50;
+document.addEventListener('keydown',(event) => {
+ //console.log(e.keycode)
+console.log("Button is pushed")
+});
 
 
-player1.style.marginLeft = margValue + "px";
-player1.style.marginLeft = player1.stylemarginLeft + "px";
+function move(e) {
 
+  if(e.keyCode == 39){
+
+        margValue += 30;
+//player1.style.left = player1style.Left + "px";
+}
 };
 
+console.log(move());
 
 
 
@@ -40,36 +33,6 @@ player1.style.marginLeft = player1.stylemarginLeft + "px";
 
 
 
-//   const Racer1 = document.QuerySelector('#Racer1')
-//       if(e.keyCode===97);
 
 
 
-//  const Racer2 = document.QuerySelector('#Racer2')
-
-
-
-
-
-
-
-
-
-// document.addEventListener('keyup', movePlayer);
-
-
-
-// //////////////////////////////////////////////////
-
-// //////////////////////////////////////////////////
-
-
-
-
-// // const checkKeyPressed = (key) => {
-// //     if (key.keyCode == '97') {
-// //         alert("The 'k' letter key has been pressed!");
-// //     }
-// // };
-
-// // window.addEventListener('keydown', checkKeyPressed, false);
