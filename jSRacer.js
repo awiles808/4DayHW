c0338285ae69cdff9f357360daaeeb508fafc87b
awiles808 <awiles808@gmail.com>
2018-03-26 23:09:20 -0600

    Updated JS keydown and Margin Left, Took out tables from HTML

--- jSRacer.js
+++ jSRacer.js
@@ -1,28 +1,61 @@
 
-
-
-
-
  document.addEventListener('DOMContentLoaded', function() { });
 
  alert("Are you ready to rumble!");
 
 
-  const Racer1 = document.QuerySelector('#Racer1')
-      if(e.keyCode===97);
-
-
-
- const Racer2 = document.QuerySelector('#Racer2')
 
 
 
 
+ alert("Are you ready to rumble!");
+
+const player1 = document.getElementById();
+let margValue = 0;
+document.addEventListener('keydown',(e) => {
+ //console.log(event keycode)
+
+  if(e.keyCode==39)
+
+    margValue += 50;
+
+
+player1.style.marginLeft = margValue + "px";
+player1.style.marginLeft = player1.stylemarginLeft + "px";
+
+};
 
 
 
 
-document.addEventListener('keyup', movePlayer);
+
+
+
+
+
+
+
+
+
+
+
+
+//   const Racer1 = document.QuerySelector('#Racer1')
+//       if(e.keyCode===97);
+
+
+
+//  const Racer2 = document.QuerySelector('#Racer2')
+
+
+
+
+
+
+
+
+
+// document.addEventListener('keyup', movePlayer);
 
 
 
