428f93caaa8b7f488680a08c01130bdeafc4c169
awiles808 <awiles808@gmail.com>
2018-03-27 22:43:47 -0600

    Up and Running With Style

--- jSRacer.js
+++ jSRacer.js
@@ -1,6 +1,7 @@
  alert("Are you ready to rumble!");
 
 const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
 let margValue = 0;
 
 document.addEventListener('keydown',(event) => {
@@ -13,17 +14,17 @@ console.log("Button is pushed")
 function move(e) {
 
   if(e.keyCode == 39){
-        margValue += 30;
+        margValue += 70;
         player1.style.left = margValue + ("px");
-        if(margValue >= 800){
-            alert("winner winner chicken dinner");
+        if(margValue >= 1000){
+            alert("Player 1 Is The Winner Winner Chicken Dinner!!");
         }
 }
    if(e.keyCode == 65){
-        margValue += 30;
+        margValue += 70;
         player2.style.left = margValue + ("px");
-        if(margValue >= 800){
-            alert("winner winner chicken dinner");
+        if(margValue >= 1000){
+            alert("Player 2 Is The Winner Winner Chicken Dinner!!");
 }
 }
 };
